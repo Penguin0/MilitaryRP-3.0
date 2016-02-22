cf32df865a17c1f2848dc789ad242dde141e2228
Penguin <unitedarms123@gmail.com>
2016-02-21 19:11:32 -0500

    Create sh_rank.lua

---
+++ sh_rank.lua
@@ -0,0 +1,116 @@
+Ranks = {}
+// Check the FAQ in order to know how to add new Ranks
+
+Ranks["Private"] = {
+	name = "Private",
+	rank = 0,
+	picture = ""
+}
+
+Ranks["Private First Class"] = {
+	name = "Private First Class",
+	rank = 1,
+	picture = Material("materials/ranks/silver-i.png")
+}
+
+Ranks["Specialist"] = {
+	name = "Specialist",
+	rank = 2,
+	picture = Material("materials/ranks/silver-ii.png")
+}
+
+Ranks["Corporal"] = {
+	name = "Corporal",
+	rank = 3,
+	picture = Material("materials/ranks/silver-iii.png")
+}
+
+Ranks["Sergeant"] = {
+	name = "Sergeant",
+	rank = 4,
+	picture = Material("materials/ranks/silver-iv.png")
+}
+
+Ranks["Master Sergeant"] = {
+	name = "Master Sergeant",
+	rank = 5,
+	picture = Material("materials/ranks/silver-elite.png")
+}
+
+Ranks["1st Sergeant"] = {
+	name = "1st Sergeant",
+	rank = 6,
+	picture = Material("materials/ranks/silver-elite-master.png")
+}
+
+Ranks["Sergeant Major"] = {
+	name = "Sergeant Major",
+	rank = 7,
+	picture = Material("materials/ranks/nova-i.png")
+}
+
+Ranks["Command Sergeant Major"] = {
+	name = "Command Sergeant Major",
+	rank = 8,
+	picture = Material("materials/ranks/gold-nova-ii.png")
+}
+
+Ranks["2nd Lieutenant"] = {
+	name = "2nd Lieutenant",
+	rank = 9,
+	picture = Material("materials/ranks/gold-nova-iii.png")
+}
+
+Ranks["1st Lieutenant"] = {
+	name = "1st Lieutenant",
+	rank = 10,
+	picture = Material("materials/ranks/gold-nova-master.png")
+}
+
+Ranks["Captain"] = {
+	name = "Captain",
+	rank = 11,
+	picture = Material("materials/ranks/master-guardian-i.png")
+}
+
+Ranks["Major"] = {
+	name = "Major",
+	rank = 12,
+	picture = Material("materials/ranks/master-guardian-ii.png")
+}
+
+Ranks["Lieutenant Colonel"] = {
+	name = "Lieutenant Colonel",
+	rank = 13,
+	picture = Material("materials/ranks/master-guardian-elite.png")
+}
+
+Ranks["Colonel"] = {
+	name = "Colonel",
+	rank = 14,
+	picture = Material("materials/ranks/distinguished-master-guardian.png")
+}
+
+Ranks["Brigadier General"] = {
+	name = "Brigadier General",
+	rank = 15,
+	picture = Material("materials/ranks/legendary-eagle.png")
+}
+
+Ranks["Major General"] = {
+	name = "Major General",
+	rank = 16,
+	picture = Material("materials/ranks/legendary-eagle-master.png")
+}
+
+Ranks["Lieutenant General"] = {
+	name = "Lieutenant General",
+	rank = 17,
+	picture = Material("materials/ranks/supreme-master-first-class.png")
+}
+
+Ranks["General"] = {
+	name = "General",
+	rank = 18,
+	picture = Material("materials/ranks/the-global-elite.png")
+}
